dc53199dcc42daec32c9e464fc3b53088c606b5a
Phil Helm <philip.helm@omgplc.com>
2015-03-03 10:55:25 +0000

    nwjs client

---
+++ public/client/nwjs/app/scripts/main.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+    if (!console) {
+        console = {};
+    }
+    var old = console.log;
+    var logger = document.getElementById('console');
+    console.log = function (message) {
+        if (typeof message === 'object') {
+            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : String(message)) + '<br />';
+        } else {
+            logger.innerHTML += message + '<br />';
+        }
+    };
+})();
+
+console.log('Node version: ' + process.version);
+
+var fs      = require('fs');
+var request = require('superagent');
+
+console.log('\ndownload-net client\n');
+
+var serverAddress = process.argv[2] || '127.0.0.1:3000';
+
+console.log('Connecting to ' + serverAddress);
+
+// request
+//   .get(serverAddress + '/task')
+//   .end(function (err, res) {
+//     console.log(res.body);
+//   });
+
+// var taskScriptPath = process.argv[2] || './test-script.js';
+// var manifestPath   = process.argv[3] || './test-manifest.txt';
+
+// console.log('Task Script  : ' + taskScriptPath);
+// console.log('Task Manifest: ' + manifestPath + '\n');
+
+// var taskScript = require(taskScriptPath);
+
+// fs.readFile(manifestPath, function (err, data) {
+//   if (err) {throw err;}
+//   taskScript.task(data);
+// });
